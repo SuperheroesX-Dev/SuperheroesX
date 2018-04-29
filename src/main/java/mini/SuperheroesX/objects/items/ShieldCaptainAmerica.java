d52366939036522f4cd3e540049229fe753fe3ef
Minecraftschurli <minecraftschurli@gmail.com>
2018-04-29 18:41:50 +0200

    .
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/main/java/mini/SuperheroesX/objects/items/ShieldCaptainAmerica.java
+++ src/main/java/mini/SuperheroesX/objects/items/ShieldCaptainAmerica.java
@@ -1,15 +1,36 @@
 package mini.SuperheroesX.objects.items;
 
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
 public class ShieldCaptainAmerica extends WeaponizedShield {
 
+    private int ticks;
+
     public ShieldCaptainAmerica() {
         super("shield_captain_america", Integer.MAX_VALUE, 8, -2.4000000953674316D, 10, 0, "");
+    }
+
+    @Override
+    public void onUsingTick(ItemStack stack, EntityLivingBase player, int count) {
+        this.ticks = count;
+    }
+
+    @Override
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft) {
+        if (ticks < 10) {
+            this.throwShield(stack, worldIn, entityLiving);
+        }
+    }
+
+    private void throwShield(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
+
     }
 
     @SubscribeEvent
